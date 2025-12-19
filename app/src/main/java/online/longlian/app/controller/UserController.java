3721005e553910749956b96ac15a6217b96aa4be
xsy <huntleonard23@gmail.com>
2025-12-19 15:56:24 +0800

    更新app包结构，完善代码生成器模块

---
+++ app/src/main/java/online/longlian/app/controller/UserController.java
@@ -0,0 +1,18 @@
+package online.longlian.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * 用户信息表 前端控制器
+ * </p>
+ *
+ * @author longlian
+ * @since 2025-12-19
+ */
+@RestController
+@RequestMapping("/app/user")
+public class UserController {
+
+}
---
+++ app/src/main/java/online/longlian/app/service/IUserService.java
@@ -0,0 +1,16 @@
+package online.longlian.app.service;
+
+import online.longlian.app.pojo.entity.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 用户信息表 服务类
+ * </p>
+ *
+ * @author longlian
+ * @since 2025-12-19
+ */
+public interface IUserService extends IService<User> {
+
+}
