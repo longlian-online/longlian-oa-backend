e53f65ade6116f25151413cf04cd7d2e2c5e64e3
xsy <huntleonard23@gmail.com>
2026-02-01 18:54:38 +0800

    feat: mysql配置druid数据库连接池

--- app/src/main/java/online/longlian/app/config/DataSourceConfig.java
+++ app/src/main/java/online/longlian/app/config/DataSourceConfig.java
@@ -1,45 +1,52 @@
 package online.longlian.app.config;
 
-import com.zaxxer.hikari.HikariDataSource;
+import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
+    @Bean
+    @ConditionalOnProperty(
+            prefix = "longlian.datasource",
+            name = "type",
+            havingValue = "sqlite"
+    )
+    public DataSource sqliteDataSource(
+            @Value("${sqlite.datasource.url}") String sqliteUrl
+    ) {
+        org.sqlite.SQLiteDataSource ds = new org.sqlite.SQLiteDataSource();
+        ds.setUrl(sqliteUrl);
+        return ds;
+    }
 
-    @Value("${longlian.datasource.type:mysql}")
-    private String datasourceType;
-
-    @Value("${DB_HOST:}")
-    private String dbHost;
-    @Value("${DB_PORT:3306}")
-    private String dbPort;
-    @Value("${DB_DATABASE:}")
-    private String dbName;
-    @Value("${DB_USERNAME:root}")
-    private String dbUser;
-    @Value("${DB_PASSWORD:}")
-    private String dbPass;
+    @Bean(destroyMethod = "close")
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConditionalOnProperty(
+            prefix = "longlian.datasource",
+            name = "type",
+            havingValue = "mysql",
+            matchIfMissing = true
+    )
+    public DruidDataSource mysqlDataSource() {
+        return new DruidDataSource();
+    }
 
     @Bean
-    public DataSource dataSource() {
-        if ("sqlite".equalsIgnoreCase(datasourceType)) {
-            org.sqlite.SQLiteDataSource ds = new org.sqlite.SQLiteDataSource();
-            ds.setUrl("jdbc:sqlite:./data/longlian.db");
-            return ds;
-        } else {
-            // MySQL + HikariCP
-            HikariDataSource ds = new HikariDataSource();
-            ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            ds.setJdbcUrl(String.format(
-                    "jdbc:mysql://%s:%s/%s?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf-8&useSSL=false",
-                    dbHost, dbPort, dbName));
-            ds.setUsername(dbUser);
-            ds.setPassword(dbPass);
-            return ds;
-        }
+    @Primary
+    @ConditionalOnProperty(
+            prefix = "longlian.datasource",
+            name = "type",
+            havingValue = "mysql",
+            matchIfMissing = true
+    )
+    public DataSource dataSource(DruidDataSource mysqlDataSource) {
+        return mysqlDataSource;
     }
 }
