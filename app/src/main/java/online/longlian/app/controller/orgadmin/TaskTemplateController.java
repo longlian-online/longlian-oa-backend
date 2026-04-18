e20534b1918e3b07a0bdf71542a9e47f92700c7e
xsy <huntleonard23@gmail.com>
2026-04-18 17:44:35 +0800

    refactor: 调整TaskTemplate和BaseTask接口，统一REST路径风格

--- app/src/main/java/online/longlian/app/controller/orgadmin/TaskTemplateController.java
+++ app/src/main/java/online/longlian/app/controller/orgadmin/TaskTemplateController.java
@@ -80,9 +80,9 @@ public class TaskTemplateController {
             summary = "启用/禁用任务模板",
             description = "禁用后无法基于该模板创建新任务流，已有任务流不受影响。status: ENABLED-启用，DISABLED-禁用"
     )
-    @PatchMapping("/task-template/status")
+    @PatchMapping("/{templateId}/status")
     @PreAuthorize("hasRole('ORG_ADMIN')")
-    public Result<Void> changeTaskTemplateStatus(@RequestBody @Valid ChangeStatusDTO changeStatusDTO) {
+    public Result<Void> changeTaskTemplateStatus(@PathVariable Long templateId, @RequestBody @Valid ChangeStatusDTO changeStatusDTO) {
         // TODO
         // taskTemplateService.changeTaskTemplateStatus(changeStatusDTO.getStatus());
         return Result.success(null);
