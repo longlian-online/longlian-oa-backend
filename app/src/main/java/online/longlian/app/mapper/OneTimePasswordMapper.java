e8a057c9c20fcdc6e08f7c1dfe6bbb0bd774e1c9
Asukai <3474790137@qq.com>
2026-04-19 10:26:49 +0800

    feat: opt 表生成

---
+++ app/src/main/java/online/longlian/app/mapper/OneTimePasswordMapper.java
@@ -0,0 +1,18 @@
+package online.longlian.app.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import online.longlian.app.pojo.entity.OneTimePassword;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 一次性密码（otp）表 Mapper 接口
+ * </p>
+ *
+ * @author longlian
+ * @since 2026-04-19
+ */
+@Mapper
+public interface OneTimePasswordMapper extends BaseMapper<OneTimePassword> {
+
+}
