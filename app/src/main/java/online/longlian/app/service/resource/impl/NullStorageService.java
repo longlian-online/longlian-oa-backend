1ef6d7ae3daf764b8b9d88e7bc8b182edc71f947
asukai <3474790137@qq.com>
2026-05-24 16:49:28 +0800

    feat: 修复包导入问题

--- app/src/main/java/online/longlian/app/service/resource/impl/NullStorageService.java
+++ app/src/main/java/online/longlian/app/service/resource/impl/NullStorageService.java
@@ -1,7 +1,7 @@
 package online.longlian.app.service.resource.impl;
 
-import online.longlian.app.pojo.bo.PresignedUploadUrlParamsBO;
-import online.longlian.app.pojo.bo.PresignedUploadUrlResultBO;
+import online.longlian.app.pojo.bo.common.PresignedUploadUrlParamsBO;
+import online.longlian.app.pojo.bo.common.PresignedUploadUrlResultBO;
 import online.longlian.app.service.resource.StorageService;
 import online.longlian.common.enumeration.StorageType;
 import org.slf4j.Logger;
