75f7fb6a8c22b202b210a475da5a734bb9f5f3a8
xsy <huntleonard23@gmail.com>
2026-07-01 17:10:40 +0800

    feat: 完善文件上传预签名接口

--- app/src/main/java/online/longlian/app/pojo/bo/common/ResourceCreateParamsBO.java
+++ app/src/main/java/online/longlian/app/pojo/bo/common/ResourceCreateParamsBO.java
@@ -2,14 +2,12 @@ package online.longlian.app.pojo.bo.common;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import online.longlian.common.enumeration.StorageType;
 
 @Data
 @AllArgsConstructor
 public class ResourceCreateParamsBO {
     private Long creatorId;
     private Long orgId;
-    private StorageType storageType;
     private String fileName;
     private String fileExt;
     private Long fileSize;
