c8d5acc3f2e27f2b9bd766bebd452f7e6cee2a7b
Asukai <3474790137@qq.com>
2026-07-28 01:23:41 +0800

    refactor(db): 移除 MySQL/SQLite 支持，PostgreSQL 作为唯一数据库
    - DataSourceConfig: 移除 MySQL/SQLite Bean 和 ConditionalOnProperty，仅保留 PG
    - MybatisPlusConfig: 移除 switch 分支，硬编码 DbType.POSTGRE_SQL
    - application.yml: 移除 MySQL/SQLite 配置段和 longlian.datasource.type 属性
    - pom.xml: 移除 mysql-connector-java 和 sqlite-jdbc 依赖
    - generator/pom.xml: mysql-connector-java -> postgresql
    - .env.test: 移除 DATASOURCE_TYPE 变量
    - Taskfile.yml: 更新描述

--- app/src/main/java/online/longlian/app/common/config/DataSourceConfig.java
+++ app/src/main/java/online/longlian/app/common/config/DataSourceConfig.java
@@ -2,60 +2,14 @@ package online.longlian.app.common.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    @Bean
-    @ConditionalOnProperty(
-            prefix = "longlian.datasource",
-            name = "type",
-            havingValue = "sqlite"
-    )
-    public DataSource sqliteDataSource(
-            @Value("${sqlite.datasource.url}") String sqliteUrl
-    ) {
-        org.sqlite.SQLiteDataSource ds = new org.sqlite.SQLiteDataSource();
-        ds.setUrl(sqliteUrl);
-        return ds;
-    }
 
     @Bean(destroyMethod = "close")
-    @Primary
-    @ConditionalOnProperty(
-            prefix = "longlian.datasource",
-            name = "type",
-            havingValue = "mysql",
-            matchIfMissing = true
-    )
-    public DruidDataSource mysqlDataSource(
-            @Value("${DB_HOST}") String host,
-            @Value("${DB_PORT}") int port,
-            @Value("${DB_DATABASE}") String database,
-            @Value("${DB_USERNAME}") String username,
-            @Value("${DB_PASSWORD}") String password
-    ) {
-        DruidDataSource ds = new DruidDataSource();
-        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://" + host + ":" + port + "/" + database + "?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=utf-8&useSSL=false");
-        ds.setUsername(username);
-        ds.setPassword(password);
-        return ds;
-    }
-
-    @Bean(destroyMethod = "close")
-    @Primary
-    @ConditionalOnProperty(
-            prefix = "longlian.datasource",
-            name = "type",
-            havingValue = "postgresql"
-    )
-    public DruidDataSource postgresqlDataSource(
+    public DruidDataSource dataSource(
             @Value("${DB_HOST}") String host,
             @Value("${DB_PORT}") int port,
             @Value("${DB_DATABASE}") String database,
--- app/src/main/java/online/longlian/app/common/config/MybatisPlusConfig.java
+++ app/src/main/java/online/longlian/app/common/config/MybatisPlusConfig.java
@@ -3,30 +3,16 @@ package online.longlian.app.common.config;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MybatisPlusConfig {
 
-    @Value("${longlian.datasource.type:postgresql}")
-    private String datasourceType;
-
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        DbType dbType = resolveDbType(datasourceType);
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(dbType));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
         return interceptor;
-    }
-
-    private DbType resolveDbType(String type) {
-        return switch (type.toLowerCase()) {
-            case "mysql" -> DbType.MYSQL;
-            case "postgresql" -> DbType.POSTGRE_SQL;
-            case "sqlite" -> DbType.SQLITE;
-            default -> throw new IllegalArgumentException("不支持的数据库类型: " + type);
-        };
     }
 }
