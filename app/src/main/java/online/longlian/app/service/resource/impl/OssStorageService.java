9e8a837377bef36b2e60ea1bd9fe0521ba5caac1
Asukai <3474790137@qq.com>
2026-09-16 02:25:31 +0800

    fix: select OSS storage constructor

--- app/src/main/java/online/longlian/app/service/resource/impl/OssStorageService.java
+++ app/src/main/java/online/longlian/app/service/resource/impl/OssStorageService.java
@@ -17,6 +17,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.net.URL;
 import java.util.*;
@@ -29,6 +30,7 @@ public class OssStorageService implements StorageService, DisposableBean {
     private final COSClient cosClient;
     private final StorageProperties.OssConfig ossConfig;
 
+    @Autowired
     public OssStorageService(StorageProperties storageProperties) {
         this(createCosClient(storageProperties.getOss()), storageProperties.getOss());
     }
