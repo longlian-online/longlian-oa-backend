0d6d7cfbba1df1d4a77c63ffae185bd41ad36e41
XSY <huntleonard23@gmail.com>
2026-09-15 13:41:50 +0800

    fix(group-application): 完善密码字段清理脚本

--- db/data-migrations/113-remove-group-application-password.sql
+++ db/data-migrations/113-remove-group-application-password.sql
@@ -1,7 +1,8 @@
--- 注册申请的密码哈希已迁移到 user.password，清理申请表中的历史凭证后删除字段。
-UPDATE group_application
-SET password = NULL
-WHERE password IS NOT NULL;
+-- 生产库的 group_application.password 由旧版本创建，先清理历史凭证再删除字段。
+-- 请在 db/schema.sql 更新后人工审核执行；Atlas 生产环境会跳过字段删除。
+UPDATE `group_application`
+SET `password` = NULL
+WHERE `password` IS NOT NULL;
 
-ALTER TABLE group_application
-    DROP COLUMN password;
+ALTER TABLE `group_application`
+    DROP COLUMN `password`;
