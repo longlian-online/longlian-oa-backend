7e80481ccd17257191c70d637b301ef05d7246fc
Asukai <3474790137@qq.com>
2026-09-15 19:58:57 +0800

    test(resource): 覆盖云存储签名与释放

--- app/src/test/java/online/longlian/app/service/resource/impl/CloudStorageServiceProbeTest.java
+++ app/src/test/java/online/longlian/app/service/resource/impl/CloudStorageServiceProbeTest.java
@@ -1,24 +1,56 @@
 package online.longlian.app.service.resource.impl;
 
 import com.qcloud.cos.COSClient;
+import com.qcloud.cos.model.GeneratePresignedUrlRequest;
 import com.qcloud.cos.model.ObjectMetadata;
 import online.longlian.app.common.exception.AppException;
 import online.longlian.app.common.properties.StorageProperties;
+import online.longlian.app.pojo.bo.common.PresignedUploadUrlParamsBO;
 import online.longlian.app.pojo.bo.common.ResourceProbeParamsBO;
 import online.longlian.app.service.resource.StorageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.test.util.ReflectionTestUtils;
 
+import java.net.URL;
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.CALLS_REAL_METHODS;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 class CloudStorageServiceProbeTest {
 
     private static final ResourceProbeParamsBO PROBE = new ResourceProbeParamsBO("resource.png", 3L, "image/png");
+    @Test
+    void shouldGenerateCloudUploadAndReadUrls() throws Exception {
+        URL signedUrl = new URL("https://cdn.example/resource.png");
+        for (CloudStorage cloud : cloudStorageServices()) {
+            when(cloud.client().generatePresignedUrl(any(GeneratePresignedUrlRequest.class))).thenReturn(signedUrl);
+
+            var upload = cloud.storage().generatePresignedUploadUrl(new PresignedUploadUrlParamsBO("resource.png"));
+            assertThat(upload.getUploadUrl()).isEqualTo(signedUrl.toString());
+            assertThat(upload.getKey()).isEqualTo("resource.png");
+            assertThat(cloud.storage().getResourceReadUrl("resource.png")).isEqualTo(signedUrl.toString());
+            assertThat(cloud.storage().getResourceReadUrls(List.of("first.png", "second.png")))
+                    .containsEntry("first.png", signedUrl.toString())
+                    .containsEntry("second.png", signedUrl.toString());
+        }
+    }
+
+    @Test
+    void shouldShutdownCloudClients() throws Exception {
+        for (CloudStorage cloud : cloudStorageServices()) {
+            ((DisposableBean) cloud.storage()).destroy();
+
+            verify(cloud.client()).shutdown();
+        }
+    }
+
 
     @Test
     void shouldAcceptCloudObjectsWithExpectedSize() {
